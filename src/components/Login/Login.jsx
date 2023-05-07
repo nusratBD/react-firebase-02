7d8913301ac91e4edaaa40fe54ff854e9304f882
nusratBD <nusrat.hurain15@gmail.com>
2023-05-07 22:11:54 +0600

    Google Signin Completed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from '../../assets/firebase/firebase.init';
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-const handleSignIn = () =>{
-    signInWithPopup(auth, provider)
-    .then(result=>{
-        const user = result.user;
-        console.log(user)
-    })
-    .catch(error=>{
-        console.log(error)
-    })
-}
 const Login = () => {
+    const [user, setUser] = useState(null);
+    const handleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setUser(user);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    const handleSignOut = () =>{
+
+    }
     return (
         <div>
-            <button onClick={handleSignIn}>Sign In With Google</button>
+            {
+                user ?
+                <div>
+                        <button onClick={handleSignOut}>Sign Out</button>
+                        <h3>User: </h3>
+                    </div>:
+                    <button onClick={handleSignIn}>Sign In With Google</button> 
+            }
         </div>
     );
 };
